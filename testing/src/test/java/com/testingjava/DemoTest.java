38b7ff5b259074f812f0525a77cf605ed3442dd7
myslfabhi <109574887+myslfabhi@users.noreply.github.com>
2022-07-19 15:27:37 +0530

    Update DemoTest.java

--- testing/src/test/java/com/testingjava/DemoTest.java
+++ testing/src/test/java/com/testingjava/DemoTest.java
@@ -22,7 +22,7 @@ public class DemoTest {
 	@Test
 	public void createDemo4Test()
 	{
-		Reporter.log("Test Demo4",true);//to print
+		Reporter.log("Test Demo4",true);//for printing we use rep.log
 	}
 
 }
