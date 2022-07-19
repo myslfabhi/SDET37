cf7b8562dbd7bf5c60bf6c5acbc536c756a30ef4
sinha <sinha@LAPTOP-DC5CTV5D>
2022-07-19 11:43:49 +0530

    1st commit

---
+++ testing/src/test/java/com/testingjava/DemoTest.java
@@ -0,0 +1,28 @@
+package com.testingjava;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void createDemo1Test()
+	{
+		Reporter.log("Test Demo1",true);
+	}
+	@Test
+	public void createDemo2Test()
+	{
+		Reporter.log("Test Demo2",true);
+	}
+	@Test
+	public void createDemo3Test()
+	{
+		Reporter.log("Test Demo3",true);
+	}
+	@Test
+	public void createDemo4Test()
+	{
+		Reporter.log("Test Demo4",true);
+	}
+
+}
---
+++ testing/src/test/java/com/testingjava/DemoTest2.java
@@ -0,0 +1,30 @@
+package com.testingjava;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+
+@Test	
+public void createDemo1()
+{
+	Reporter.log("test demo1",true);
+}
+@Test
+public void practiceTest1()
+{
+	String url = System.getProperty("URL");
+	String username = System.getProperty("USERNAME");
+	String password = System.getProperty("PASSWORD");
+	
+	System.out.println(System.getProperty("URL"));
+	System.out.println(System.getProperty("USERNAME"));
+	System.out.println(System.getProperty("PASSWORD"));
+}
+{
+	
+}
+
+	
+
+}
